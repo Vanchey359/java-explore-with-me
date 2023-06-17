e36c17048919f364f1903327ac6a4945f7a947c0
ivan Averin <twink.averin@yandex.ru>
2023-06-17 15:00:59 +0300

    stats server added

---
+++ stats-service/stats-server/src/main/java/ru/practicum/controller/StatsController.java
@@ -0,0 +1,36 @@
+package ru.practicum.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.dto.EndpointHitDto;
+import ru.practicum.dto.ViewStatsDto;
+import ru.practicum.service.StatsService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class StatsController {
+    private final StatsService statsService;
+
+    @PostMapping("/hit")
+    public ResponseEntity<EndpointHitDto> addStatistic(@RequestBody EndpointHitDto endpointHitDto) {
+        log.info("Adding endpointHitDto {}", endpointHitDto);
+        return ResponseEntity.ok(statsService.addStatistic(endpointHitDto));
+    }
+
+    @GetMapping("/stats")
+    public ResponseEntity<List<ViewStatsDto>> getStatistic(
+            @RequestParam(name = "start") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+            @RequestParam(name = "end") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+            @RequestParam(name = "uris", required = false) List<String> uris,
+            @RequestParam(name = "unique", defaultValue = "false") Boolean unique) {
+        log.info("Getting statistic from start {}. end {}. uris {}, unique {}", start, end, uris, unique);
+        return ResponseEntity.ok(statsService.getStatistic(start, end, uris, unique));
+    }
+}
---
+++ stats-service/stats-server/src/main/java/ru/practicum/model/EndpointHit.java
@@ -0,0 +1,36 @@
+package ru.practicum.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "ENDPOINT_HIT")
+public class EndpointHit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String app;
+
+    private String uri;
+
+    private String ip;
+
+    @Column(name = "created")
+    private LocalDateTime timestamp;
+}
