cb02d54b0e7e16524624de73b61d1a15e4bf6f03
ivan Averin <twink.averin@yandex.ru>
2023-07-08 14:09:39 +0300

    checkstyle fix

--- ewm-service/src/main/java/ru/practicum/ewmService/service/comment/CommentServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/ewmService/service/comment/CommentServiceImpl.java
@@ -2,7 +2,6 @@ package ru.practicum.ewmService.service.comment;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
